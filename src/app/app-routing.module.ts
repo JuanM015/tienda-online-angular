5456b0cbabc30ae51943480215b995a96c796b34
JuanM015 <JuanM015.juanmontoya015@gmail.com>
2019-03-21 15:55:15 -0500

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { Page404Component } from './components/errors/page404/page404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent
+  },
+  {
+    path:'**',
+    component: Page404Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+// 10031632 - Contraseña del cucho
